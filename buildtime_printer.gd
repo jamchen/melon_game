287f0af05d29ebb2d014543a05ed32bfc6c8e21b
ken19800813 <65639595+ken19800813@users.noreply.github.com>
2025-04-01 20:48:56 +0800

    Add files via upload

--- buildtime_printer.gd
+++ buildtime_printer.gd
@@ -10,7 +10,3 @@ func _process(_delta):
 		if ProjectSettings.get_setting("application/config/build_datetime") != t:
 			ProjectSettings.set_setting("application/config/build_datetime", t)
 			ProjectSettings.save()
-
-func _on_shake_button_pressed():
-	for fruit in get_tree().get_nodes_in_group("fruits"):  # 遍歷所有水果
-		fruit.shake()
